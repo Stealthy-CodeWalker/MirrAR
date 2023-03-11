ede37385b67ada599229d9746037b7064f8753eb
ritveek-rana <ritveekrana@gmail.com>
2023-03-11 11:36:42 +0530

    take

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,6 @@ import trial from "./components/trial.vue"
 import sdk from "./components/sdk.vue";
 </script>
 
-<style>
-    html,
-    body {
-      height: 100%;
-      width: 100%;
-      margin: 0;
-      overflow: hidden;
-    }
-
-    #MirrAR-panel {
-      height: 100%;
-      width: 100%;
-      top: 0;
-      position: absolute;
-      z-index: 0;
-    }
-
-  </style>
-
 <template>
     <sdk>
         <trial/>
--- src/components/trial.vue
+++ src/components/trial.vue
@@ -1,11 +1,30 @@
 <script setup>
+  var tryonPayload = {
+      product_id: YOUR_PRODUCT_SKU, // Must required
+      product_category: "NECK",
+      neck_wearing: NECKLACE_URL, // The image should be a transparent png
+      x_neck: null, // Horizontal Offset (optional)
+      y_neck: null, // Vertical Offset (optional)
+      ppu_neck: null, // On increasing the ppu value the tryon image gets smaller (optional)
+    };
 
-
+    $(document).ready(function () {
+      var options = {
+          key: "2rFXetD2QxDEGWBJtFyxOwHOYjfZuo36vtsAvuZA",
+          ExID: "VTO",
+          brandLogo: BRAND_LOGO_URL
+      };
+      init(options);
+      start();
+    });
+    document.addEventListener("init", function () {
+      filterAddRemove("add", tryonPayload);
+    });
 </script>
 
 <template>
   <div>
-    Hi
+    {{ tryonPayload }}
   </div>
 </template>
 
