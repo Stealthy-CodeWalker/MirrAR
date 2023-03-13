fdee0666c13f97aec61bde16786487c7fd619447
ritveek-rana <ritveekrana@gmail.com>
2023-03-13 10:15:58 +0530

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import trial from "./components/trial.vue"
 import sdk from "./components/sdk.vue";
+import panel from "./components/panel.vue";
 </script>
 
 <template>
     <sdk>
-        <trial/>
+        <panel>
+            <trial/>
+        </panel>
     </sdk>
 </template>
 
