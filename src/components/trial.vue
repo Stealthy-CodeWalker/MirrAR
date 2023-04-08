b70ed4f0094a5dfa4d92e1cc3f4a3fcfaf46915b
Stealthy-CodeWalker <webdev.vanguard@gmail.com>
2023-04-08 08:59:00 +0530

    take 10

--- src/components/trial.vue
+++ src/components/trial.vue
@@ -1,25 +1,25 @@
 <script setup>
-   var tryonPayload = {
-      product_id: YOUR_PRODUCT_SKU, // Must required
-      product_category: "NECK",
-      neck_wearing: NECKLACE_URL, // The image should be a transparent png
-      x_neck: null, // Horizontal Offset (optional)
-      y_neck: null, // Vertical Offset (optional)
-      ppu_neck: null, // On increasing the ppu value the tryon image gets smaller (optional)
-    };
+    var tryonPayload = {
+    product_id: "SM21-SH-M02-RD-S-001", // Must required
+    product_category: "NECK",
+    neck_wearing: "https://www.dropbox.com/s/dv4z4ldgx2lh6ff/1.png?dl=0", // The image should be a transparent png
+    x_neck: null, // Horizontal Offset (optional)
+    y_neck: null, // Vertical Offset (optional)
+    ppu_neck: null, // On increasing the ppu value the tryon image gets smaller (optional)
+  };
 
-    var a=(document).ready(function () {
-      const options = {
-            "key": "2rFXetD2QxDEGWBJtFyxOwHOYjfZuo36vtsAvuZA",
-            //"ExID": "Parent_Container_ID", // Optional
-            "brandLogo": "Watermark_URL" // Optional
-      };
-      init(options);
-      start();
-    });
-    var b=document.addEventListener("init", function () {
-      filterAddRemove("add", tryonPayload);
-    });
+  $(document).ready(function () {
+    var options = {
+      "key": "2rFXetD2QxDEGWBJtFyxOwHOYjfZuo36vtsAvuZA",
+      //"ExID": "Parent_Container_ID", // Optional
+      "brandLogo": "Watermark_URL" // Optional
+    };
+    init(options);
+    start();
+  });
+  document.addEventListener("init", function () {
+    filterAddRemove("add", tryonPayload);
+  });
 </script>
 
 <template>
