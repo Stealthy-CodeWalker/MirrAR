e89db5ab72d46f2812518e22c4fa2f3aa966aec6
Stealthy-CodeWalker <webdev.vanguard@gmail.com>
2023-04-12 20:25:42 +0530

    take 23

--- src/components/trial.vue
+++ src/components/trial.vue
@@ -2,7 +2,7 @@
     var tryonPayload = {
     product_id: "SM21-SH-M02-RD-S-001", // Must required
     product_category: "NECK",
-    neck_wearing: "./1.png", // The image should be a transparent png
+    neck_wearing: "https://mirr-ar-git-main-stealthy-codewalker.vercel.app/1.png", // The image should be a transparent png
     x_neck: null, // Horizontal Offset (optional)
     y_neck: null, // Vertical Offset (optional)
     ppu_neck: null, // On increasing the ppu value the tryon image gets smaller (optional)
